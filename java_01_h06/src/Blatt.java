36d1998ea80586fa63c770fabd331b3e51e7d8c1
JustinBaumeyer <justin.baumeyer@pm.me>
2020-11-18 16:07:05 +0100

    Added comments and optimized Blatt.getMaxDuplicates() method

--- java_01_h06/src/Blatt.java
+++ java_01_h06/src/Blatt.java
@@ -4,15 +4,20 @@ import java.util.stream.IntStream;
 
 public class Blatt {
 	private int[] values;
-	
+
+	/**
+	 * Konstruktor der Klasse Blatt.
+	 * @param v Symbolisiert die Werte der Karten
+	 */
 	public Blatt(int[] v) {
 		this.setValues(v);
 	}
 
-	public int[] getValues() {
-		return values;
-	}
-
+	/**
+	 * Setter des Attributs values, welcher überprüft, ob die Länge des Arrays genau 3 beträgt. Falls dem nicht so ist,
+	 * wird eine IllegalArgumentException geworfen!
+	 * @param values
+	 */
 	public void setValues(int[] values) {
 		if(values.length != 3) {
 			throw new IllegalArgumentException("Length of v must be 3!");
@@ -20,29 +25,44 @@ public class Blatt {
 		this.values = values;
 	}
 
-	
+	/**
+	 * Gibt ein Entry zurück, welcher die Information enthält, welche Karte wie oft in der Hand enthalten ist.
+	 * Bsp:
+	 * Hand: [5,5,9]
+	 * Return: {5=2,9=1}
+	 * @return
+	 */
 	public Map.Entry<Integer,Integer> getMaxDuplicates() {
 		Map<Integer, Integer> res = new HashMap<>();
-		for(int i : this.values) {
-			if(res.containsKey(i)) {
-				res.put(i,res.get(i)+1);
-			} else {
-				res.put(i,1);
-			}
-		}
+		IntStream.of(this.values).forEach(i -> {
+				res.put(i,Optional.ofNullable(res.get(i)).orElse(0)+1);
+		});
 		return res.entrySet().stream().max(Map.Entry.comparingByValue()).get();
 	}
 
+	/**
+	 * Gibt den Wert der Karte zurück, die nicht im Paar zu finden ist. Falls es ein Drilling ist, wird
+	 * einfach der Wert der Karte ausgegeben, aus welchem der Drilling besteht, da sonst eine ArrayIndexOutOfBoundsException droht.
+	 * @return
+	 */
 	public int getAdditionalCard() {
 		Map.Entry<Integer,Integer> res = getMaxDuplicates();
-		if(res.getValue() == 3) return res.getKey();
+		if(res.getValue() == values.length) return res.getKey();
 		return IntStream.of(this.values).filter(x -> x != res.getKey()).limit(1).toArray()[0];
 	}
 
+	/**
+	 * Gibt die Summe der Einträge im values Array zurück.
+	 * @return
+	 */
 	public int getSum() {
 		return IntStream.of(this.values).sum();
 	}
-	
+
+	/**
+	 * Gibt das Objekt Blatt als String aus, welcher aus den Komma-Separierten Einträgen des Attributs values besteht.
+	 * @return
+	 */
 	@Override
 	public String toString() {
 		return IntStream.of(this.values).mapToObj(Integer::toString).collect(Collectors.joining(", "));
--- java_01_h06/src/BlattVergleich.java
+++ java_01_h06/src/BlattVergleich.java
@@ -3,6 +3,16 @@ import java.util.Map;
 
 public class BlattVergleich implements Comparator<Blatt> {
 
+    /**
+     * Vergleicht das Blatt von zwei Spielern unter berücksichtigung der gegegeben Kriterien:
+     *  Drilling schlägt Paar, Paar schlägt komplett unterschiedliche Karten.
+     *  Bei 2 Drillingen bzw. Paaren zählt das mit dem höheren Wert.
+     *  Bei 2 Paaren mit gleichem Wert zählt die 3. Karte.
+     *  Bei komplett unterschiedlichen Karten zählt die Summe der drei Karten.
+     * @param b1 Blatt 1
+     * @param b2 Blatt 2
+     * @return
+     */
     @Override
     public int compare(Blatt b1, Blatt b2) {
         Map.Entry<Integer,Integer> maxDuplicates1 = b1.getMaxDuplicates();
