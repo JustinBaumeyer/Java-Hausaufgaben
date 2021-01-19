357b478174fe9f5c89324d2c1742341e165347bc
Justin Baumeyer <justin.baumeyer@pm.me>
2021-01-19 22:58:57 +0100

    Commited h13

--- java_01_h13/src/code/Huffman.java
+++ java_01_h13/src/code/Huffman.java
@@ -1,22 +1,32 @@
 package code;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Collections;
 import java.util.List;
 
 public class Huffman {
+    /**
+     * Die Datei f wird eingelesen und der zu entschlüsselnde Text wird mit hilfe der Dekodier-Tabelle entschlüsselt
+     * @param f Datei mit verschlüsseltem Text und Dekodier-Tabelle
+     * @return Entschlüsselter Text
+     * @throws IllegalArgumentException Es wird eine IllegalArgumentException geworfen, wenn die Datei f nicht das richtige Format hat.
+     * @throws RuntimeException Es wird eine RuntimeException geworfen, falls beim Laden der Datei ein Fehler auftritt.
+     */
     public static String decode(File f) {
         StringBuilder result = new StringBuilder();
-        List<String> lines = Collections.emptyList();
+        List<String> lines;
+
+        //Die Zeilen in der Datei f werden in eine Liste geladen.
         try {
             lines = Files.readAllLines(f.toPath(), StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
+            throw new RuntimeException("Beim Laden der Datei ist ein Fehler aufgetreten");
         }
 
+        //Die Anforderung an die Datei ist, dass sie aus 28 Zeilen besteht
         if (lines.size() != 28)
             throw new IllegalArgumentException("Die Datei entspricht nicht dem gewünschten Dateiformat!");
 
@@ -24,6 +34,7 @@ public class Huffman {
         Node root = buildTree(lines.subList(1, lines.size()));
 
         Node current = root;
+        //Laueft den Baum solange durch, bis die aktuelle Kodierung des Buchstabens an einem Blatt ankommt und fuegt dessen Buchstaben an das Ergebnis hinzu.
         for (char c : encrypted) {
             if (c == '0' && current.getLeft(false) != null) current = current.getLeft(false);
             if (c == '1' && current.getRight(false) != null) current = current.getRight(false);
@@ -35,6 +46,9 @@ public class Huffman {
         return result.toString();
     }
 
+    /**
+     * Klasse Node fuer die Elemente des Baumes
+     */
     private static class Node {
         private char ch;
         private Node left, right;
@@ -52,27 +66,44 @@ public class Huffman {
             this.ch = c;
         }
 
+        /**
+         * @param create Gibt an, ob die Node erstellt werden soll, wenn sie noch nicht existiert.
+         * @return Gibt die linke Node zurueck.
+         */
         public Node getLeft(boolean create) {
             if (create && left == null) this.left = new Node(null, null);
             return left;
         }
 
+        /**
+         * @param create Gibt an, ob die Node erstellt werden soll, wenn sie noch nicht existiert.
+         * @return Gibt die rechte Node zurueck.
+         */
         public Node getRight(boolean create) {
             if (create && right == null) this.right = new Node(null, null);
             return right;
         }
 
+        /**
+         * @return Gibt zurueck, ob die Node ein Blatt ist. Dies ist der Fall, wenn sie selbst keine Kinder hat.
+         */
         private boolean isLeaf() {
             return (this.getLeft(false) == null) && (this.getRight(false) == null);
         }
     }
 
+    /**
+     * Wandelt die in einer Liste uebergebene Dekodier-Tabelle in einen Baum um, der fuer die Entschluesselung genutzt wird.
+     * @param list Dekodier-Tabelle
+     * @return Dekodier-Baum
+     */
     private static Node buildTree(List<String> list) {
         Node root = new Node(null, null);
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).length() > 0) {
                 String code = list.get(i);
                 Node current = root;
+                //Geht die Kodierung des jeweiligen Buchstaben solange durch, bis man am Blatt angekommen ist und setzt dessen Char auf den jeweiligen Buchsstaben bzw Leerzeichen.
                 for (char c : code.toCharArray()) {
                     if (c == '0') current = current.getLeft(true);
                     else if (c == '1') current = current.getRight(true);
